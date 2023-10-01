0458bf0d24395cb6595e35258d918021011af58b
FCC <kartheeswaran07v@gmail.com>
2023-10-01 23:24:55 +0530

    CORS Issue

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-jgbgc@z301m9hfm_@$b^voxv!w#ci)ma-d6qu!n51m9ze#0tej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hydrasaveAPI',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'api.urls'
